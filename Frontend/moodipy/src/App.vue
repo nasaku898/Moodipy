841b0ff4e903895ac279452ec0a1516c5ac94fd8
Simon Lim <simonlim898@hotmail.com>
2021-03-28 15:24:59 -0400

    #03 Setup basic css

--- Frontend/moodipy/src/App.vue
+++ Frontend/moodipy/src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MusicPage></MusicPage>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MusicPage from './components/MusicPage/MusicPage'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    MusicPage
+  },
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /*font-family: Avenir, Helvetica, Arial, sans-serif;*/
+  font-family: Montserrat;
+  font-style: normal;
+  /*
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #2c3e50;*/
+  width: 100%;
+  height: 100vh;
+  background-image: url(./assets/Frame@2x.png);
+  background-size: 2100px 1180px;
 }
 </style>
