e7687a880e0b8ed5f227899030c8ebcab96caeaa
Lauren Lim <mnxnhl@gmail.com>
2021-03-28 21:17:02 -0400

    #2 adding onboarding css

--- Frontend/moodipy/src/App.vue
+++ Frontend/moodipy/src/App.vue
@@ -1,50 +1,40 @@
 <template>
-<<<<<<< HEAD
-<div>
-  <!--  <img alt="Vue logo" src="./assets/logo.png">-->   
-  <OnboardingPage msg="Welcome to Your Vue.js App"/>
-</div>
-  
+  <OnboardingPage></OnboardingPage>
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld.vue'
-import OnboardingPage from './components/OnboardingPage/OnboardingPage.vue'
-=======
-  <MusicPage></MusicPage>
-</template>
-
-<script>
-import MusicPage from './components/MusicPage/MusicPage'
->>>>>>> 841b0ff4e903895ac279452ec0a1516c5ac94fd8
-
+import OnboardingPage from "./components/OnboardingPage/OnboardingPage.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-<<<<<<< HEAD
-    // HelloWorld,
-    OnboardingPage
-  }
-=======
-    MusicPage
+    OnboardingPage,
   },
->>>>>>> 841b0ff4e903895ac279452ec0a1516c5ac94fd8
-}
+};
 </script>
 
 <style>
+@media only screen and (min-width: 768px) {
+  #app {
+    /*font-family: Avenir, Helvetica, Arial, sans-serif;*/
+    /*
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;*/
+    width: 100%;
+    height: 100vh;
+    background-image: url(./assets/Frame@2x.png);
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+  }
+}
+@media only screen and (max-width: 768px) {
+  #app {
+    background: #ffffff;
+  }
+}
 #app {
-  /*font-family: Avenir, Helvetica, Arial, sans-serif;*/
   font-family: Montserrat;
   font-style: normal;
-  /*
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;*/
-  width: 100%;
-  height: 100vh;
-  background-image: url(./assets/Frame@2x.png);
-  background-size: 2100px 1180px;
 }
 </style>
