bdd559ffc158b5902a7721859f717595549ef948
Judy Mezaber <judymezaber1699@gmail.com>
2021-04-10 12:46:24 -0400

    Creating a Button and Input component

---
+++ Frontend/moodipy/src/components/Button/Button.vue
@@ -0,0 +1,42 @@
+<template>
+<button id="my-btn" :style="{background: background, color: color}">{{ text }}</button>
+</template>
+
+<script>
+export default {
+    name: "Button",
+    props: {
+        text: String,
+        background: String,
+        color: String
+    }
+}
+</script>
+
+<style scoped>
+#my-btn {
+
+    /* button positioning and style */
+
+    display: inline-block;
+    padding: 10px 20px;
+    align-items: center;
+    border: 2px solid #6E41E2;
+    
+    box-shadow: 3px 3px rgba(0, 0, 0, 0.1);
+
+    box-sizing: border-box;
+    cursor: pointer;
+
+    /* text */
+    border-radius: 4px;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 150%;
+    margin-right: 20px;
+    text-align: center;
+
+}
+</style>
---
+++ Frontend/moodipy/src/components/Input/Input.vue
@@ -0,0 +1,36 @@
+<template>
+<input id="input-box">
+</template>
+
+<script>
+export default {
+    name: "Input"
+}
+</script>
+
+<style>
+#input-box {
+    background: #FFFFFF;
+    /* Shadow */
+    box-shadow: 0px 4px 4px rgba(51, 51, 51, 0.04), 0px 4px 16px rgba(51, 51, 51, 0.08);
+    border-radius: 4px;
+
+    /* input */
+    width: 482px;
+    height: 56px;
+
+    /* text inside the input  */
+
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 150%;
+    display: flex;
+    align-items: center;
+
+    /* Colors */
+    color: black;
+    padding-left: 10px;
+}
+</style>
