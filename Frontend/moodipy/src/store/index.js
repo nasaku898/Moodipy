258ea6f53c1a6273f9b539c25f540bcfda687a98
Simon Lim <simonlim898@hotmail.com>
2021-04-12 12:49:59 -0400

    #13 remove comment

--- Frontend/moodipy/src/store/index.js
+++ Frontend/moodipy/src/store/index.js
@@ -1,12 +1,4 @@
 import { createStore } from 'vuex'
-// import Vuex from 'vuex'
-// import Vue from 'vue'
-
-// Vue.use(Vuex)
-
-// export default new Vuex.Store({
-
-// })
 
 export default createStore({
     state: {
