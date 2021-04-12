c5840d80fdb58e59696e96796130de14fdc2e3de
Simon Lim <simonlim898@hotmail.com>
2021-04-12 18:25:52 -0400

    update dist

--- Frontend/moodipy/server.js
+++ Frontend/moodipy/server.js
@@ -6,6 +6,10 @@ const app = express()
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
+app.get(/.*/, (req,res)=>{
+    res.sendFile(__dirname + "/dist/index.html")
+})
+
 const port = process.env.PORT || 8080
 
 app.listen(port, () => { console.log("Server started") })
